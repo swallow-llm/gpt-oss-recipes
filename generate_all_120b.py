b0c3887aec4f607753838cd8251515fedcbe7d99
Vaibhavs10 <vaibhavs10@gmail.com>
2025-08-03 00:24:02 +0200

    up

---
+++ generate_all_120b.py
@@ -0,0 +1,39 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+import torch
+from transformers.distributed import DistributedConfig
+
+model_path = "/fsx/vb/new-oai/gpt-oss-120b-trfs"
+tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left")
+
+# Set up chat template
+messages = [
+     {"role": "user", "content": "Explain how expert parallelism works in large language models."}
+]
+chat_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
+
+generation_config = GenerationConfig(
+    max_new_tokens=1024,
+    do_sample=True,
+)
+
+device_map = {
+    "distributed_config": DistributedConfig(enable_expert_parallel=1),  # Enable Expert Parallelism
+    "tp_plan": "auto",  # Enables Tensor Parallelism
+}
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype="auto",
+    attn_implementation="vllm-flash-attn3:flash_attn_varlen_func",
+    **device_map,
+)
+
+model.eval()
+
+# Tokenize and generate
+inputs = tokenizer(chat_prompt, return_tensors="pt").to("cuda")
+outputs = model.generate(**inputs, generation_config=generation_config)
+
+# Decode and print
+response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print("Model response:", response.split("assistant\n")[-1].strip())
