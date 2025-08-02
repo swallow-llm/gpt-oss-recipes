2d693e16379d65b1e27075b9f65a7bd41977f74b
Vaibhavs10 <vaibhavs10@gmail.com>
2025-08-03 00:21:43 +0200

    up

---
+++ generate_flash_attention_20b.py
@@ -0,0 +1,32 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+
+model_path = "/fsx/vb/new-oai/gpt-oss-20b-trfs"
+tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="left")
+
+messages = [
+    {"role": "user", "content": "Explain tensor parallelism in simple terms."}
+]
+chat_prompt = tokenizer.apply_chat_template(messages, tokenize=False)
+
+generation_config = GenerationConfig(
+    max_new_tokens=1024,
+    do_sample=True,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype="auto",
+    attn_implementation="ft-hf-o-c/vllm-flash-attn3:flash_attn_varlen_func", # Flash Attention with Sinks
+    device_map="auto",
+)
+
+model.eval()
+
+# Tokenize and generate
+inputs = tokenizer(chat_prompt, return_tensors="pt").to("cuda")
+outputs = model.generate(**inputs, generation_config=generation_config)
+
+# Decode and print
+response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print("Model response:", response.split("assistant\n")[-1].strip())
