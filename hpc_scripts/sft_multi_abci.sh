2d4082b2a98a68f26b0b4715c212608e8d58cb19
S. Ota <1632335+susumuota@users.noreply.github.com>
2025-08-21 19:26:40 +0900

    feat: add support for flash attention 3

--- hpc_scripts/sft_multi_abci.sh
+++ hpc_scripts/sft_multi_abci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q rt_HF
 #PBS -l select=2
-#PBS -l walltime=8:00:00
+#PBS -l walltime=1:00:00
 #PBS -N sft_multi_gpt_oss_20b
 #PBS -k oe
 #PBS -j oe
@@ -23,26 +23,29 @@ module load cuda/12.8/12.8.1
 # source .venv/bin/activate
 
 # uv pip install "torch>=2.8.0" --index-url https://download.pytorch.org/whl/cu128
-# uv pip install "trl>=0.21.0" "peft>=0.17.0" "transformers>=4.55.2" trackio
-# uv pip install deepspeed
+# uv pip install "trl>=0.21.0" "peft>=0.17.0" "transformers>=4.55.2" "triton>=3.4" "kernels>=0.9.0"
+# uv pip install accelerate deepspeed trackio wandb
+# hf auth login
+# wandb login
 
 # shellcheck source=/dev/null
 source .venv/bin/activate
 
 export NUM_MACHINES=$(cat "${PBS_NODEFILE}" | uniq | wc -l)
-export NUM_PROCESSES=$(( NUM_MACHINES * 8 ))
-export JOBID=$(echo "${PBS_JOBID}" | cut -d '.' -f 1)
+export NUM_GPUS=$(nvidia-smi -L | grep -c "^GPU")
+export NUM_PROCESSES=$(( NUM_MACHINES * NUM_GPUS ))
+export JOB_ID=$(echo "${PBS_JOBID}" | cut -d '.' -f 1)
 export MASTER_ADDR=$(cat "${PBS_NODEFILE}" | uniq | head -n 1)
-export MASTER_PORT=$(( 50000 + JOBID % 10000 ))
+export MASTER_PORT=$(( 50000 + JOB_ID % 10000 ))
+export HOSTFILE="hostfile.${JOB_ID}"
 
 echo "NUM_MACHINES: ${NUM_MACHINES}"
+echo "NUM_GPUS: ${NUM_GPUS}"
 echo "NUM_PROCESSES: ${NUM_PROCESSES}"
-echo "JOBID: ${JOBID}"
+echo "JOB_ID: ${JOB_ID}"
 echo "MASTER_ADDR: ${MASTER_ADDR}"
 echo "MASTER_PORT: ${MASTER_PORT}"
-
-export HOSTFILE="hostfile.${JOBID}"
-echo "HOSTFILE: $HOSTFILE"
+echo "HOSTFILE: ${HOSTFILE}"
 
 awk '{print $0 " slots=8"}' "${PBS_NODEFILE}" > "${HOSTFILE}"
 cat "${HOSTFILE}"
@@ -58,4 +61,7 @@ accelerate launch \
     --rdzv_backend c10d \
     --max_restarts 0 \
     sft.py \
-    --config configs/sft_full.yaml
+    --config configs/sft_full.yaml \
+    --attn_implementation kernels-community/vllm-flash-attn3 \
+    --report_to wandb \
+    --max_length 16384
